d7ef0301b5f9e1a8e7a32c7a3124adae3f2fb291
Dusan Dimic <dusan.dimic@vegait.rs>
2025-01-14 14:17:35 +0100

    Adds logic for authenticating users

---
+++ src/main/java/com/aibou/security/config/JwtService.java
@@ -0,0 +1,72 @@
+package com.aibou.security.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "7363569a22e2b53024472a42d44576f6db996846d0024b9dc2f6aeb5545a76bab00e77388afb70fe32573e4c568d16f35b8a9438b784011d3308f312d4642ba7e1bbbc8a5f6aec7f8a327a727779bacd4acd87acdd216b35f100c6956d1f1be2d687c71dcfd5e562fd14440ac25139cb9380b42585ab77dad9291271142e14435ccf52ba26fc6bf70e589a1b2351388de3269e0c9a6fce5506edb864d92db5e0c22604338413299aa1a23147af4b75d3a12faacd51ce16c88ff214f05740c5b5738b735ecee33714c0a0d9d411526cac2a1b056600a39177ac73b6791eb7a24a2ff908ca3637881cc27581f54102b2a567456de405eda091c56bffadb6f4d005";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(
+            Map<String, Object> extractClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts.builder()
+                .claims(extractClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), Jwts.SIG.HS256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
---
+++ src/main/java/com/aibou/security/user/Role.java
@@ -0,0 +1,6 @@
+package com.aibou.security.user;
+
+public enum Role {
+    USER,
+    ADMIN,
+}
